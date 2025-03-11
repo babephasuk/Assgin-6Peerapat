b5cf4b67cdebdf821caeb4362e99b0929efdadb9
babephasuk <139837619+babephasuk@users.noreply.github.com>
2025-03-11 15:12:51 +0700

    initial commit and add index.html

---
+++ script.js
@@ -0,0 +1,22 @@
+let items = [];
+
+function addItem() {
+    let input = prompt("กรอกข้อมูลที่ต้องการเพิ่มลงในรายการ:");
+    
+    if (input !== null && input !== "") {
+        items.push(input);
+    }
+
+    displayList();
+}
+
+function displayList() {
+    let listElement = document.getElementById("list");
+    listElement.innerHTML = "";
+
+    for (let i = 0; i < items.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = items[i];
+        listElement.appendChild(li);
+    }
+}
